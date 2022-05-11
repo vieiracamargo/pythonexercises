2b3ccf8e2ed44782b8cc0bfd21df4f0374a8b854
vieiracamargo <brunovieiracamargo@gmail.com>
2022-05-11 10:39:29 -0300

    utilizando operadores aritimeticos

---
+++ ex006.py
@@ -0,0 +1,7 @@
+"""Faça um programa que leai um número inteiro e mostre
+na tela seu sucessor e seu antecessor"""
+
+number = int(input("Digite um número: "))
+
+print(f"O número antecessor de {number} é: {number-1}")
+print(f"O número sucessor de {number} é: {number+1}")
