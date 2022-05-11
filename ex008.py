afb371eccd09de4438b09505c36655294a5d1511
vieiracamargo <brunovieiracamargo@gmail.com>
2022-05-11 11:33:55 -0300

    Calculando a média de um aluno

---
+++ ex008.py
@@ -0,0 +1,11 @@
+"""
+Desenvolva um programa que leia as duas notas de um aluno,
+calcule  e mostre a sua média.
+"""
+
+grade1 = float(input("Digite a primeira nota: "))
+grade2 = float(input("Digite a segunda nota: "))
+
+average = (grade1 + grade2) / 2
+
+print(f"A média do aluno é: {average:.2f}")
