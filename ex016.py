2fe9a18289159bf489ab12c0883fdccb6ae14b0d
vieiracamargo <brunovieiracamargo@gmail.com>
2022-05-13 14:35:06 -0300

    Calculando seno, cosseno e tangente

---
+++ ex016.py
@@ -0,0 +1,8 @@
+import math
+
+angle = float(input("Digite um angulo: "))
+
+print(f"O cosseno do angulo é: {math.cos(angle)}")
+print(f"O seno do angulo é: {math.sin(angle)}")
+print(f"A tangente do angulo é: {math.tan(angle)}")
+
